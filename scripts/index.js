24403f9013191f4b34971977d99b97aedba9d4fa
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2020-06-28 19:10:45 +1000

    JS обработчик добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,149 @@
 // Объявление переменных
-let editButton = document.querySelector('.profile__button-edit');
-let closeButton = document.querySelector('.popup__close-icon');
-let submitButton = document.querySelector('.popup__button-submit');
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__item_input-name');
-let jobInput = formElement.querySelector('.popup__item_input-about');
+let editButton = document.querySelector('.profile__button-edit');
+let editPopup = document.querySelector('.popup__edit');
+let submitButton = document.querySelector('.popup__button-submit');
+let formEdit = document.querySelector('.popup__input_container-edit');
+let formAdd = document.querySelector('.popup__input_container-add');
+let nameInput = formEdit.querySelector('.popup__item_input-name');
+let jobInput = formEdit.querySelector('.popup__item_input-about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-//let likeButton = document.querySelector('.elements__button');
 
-//Выводим данные пользователя в форму при открытии модального окна редактировать профиль
-function popupOpen() {
-    popup.classList.add('popup_open');
+//функция принимает элемент попапа в качестве аргумента
+function togglePopup (popup) {
+    popup.classList.toggle('popup_open');
+}
+
+//Открываем попап редактирования
+editButton.addEventListener('click', function () {
+    togglePopup(editPopup);
     nameInput.value = profileName.textContent;
     jobInput.value = profileAbout.textContent;
-}
+});
 
-//Закрытие модального окна 
-function popupClose () {
-    popup.classList.remove('popup_open');
-}
+//Открываем попап добавления карточки
+let addButton = document.querySelector('.profile__button-add');
+let addPopup = document.querySelector('.popup__add');
+addButton.addEventListener('click', function () {
+    togglePopup(addPopup);
+});
+
+
+
+
+//Закрываем модальное окно редактирования 
+let closeButtonEdit = document.querySelector('.popup__close_edit');
+closeButtonEdit.addEventListener('click', function () {
+    togglePopup(editPopup);
+});
+//Закрываем модальное окно добавления карточки
+let closeButtonAdd = document.querySelector('.popup__close_add');
+closeButtonAdd.addEventListener('click', function () {
+    togglePopup(addPopup);
+});
 
 //Обработчик отправки формы
-function formSubmitHandler (evt) {
+function editFormSubmitHandler (evt) {
     evt.preventDefault();  
     profileName.textContent = nameInput.value;
     profileAbout.textContent = jobInput.value;
-    popupClose ();
+    togglePopup(editPopup);
+}
+//Прикрепляем обработчик к форме
+formEdit.addEventListener('submit', editFormSubmitHandler);
+
+
+
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const templateElements = document.querySelector('.elements-template').content;
+const listElements = document.querySelector('.elements__list');
+
+function addCard(name, link) {
+    const element = templateElements.cloneNode(true);
+    element.querySelector('.elements__title').textContent = name;
+    element.querySelector('.elements__image').src = link;
+
+    element.querySelector('.button__delete').addEventListener('click', deleteCard);
+    element.querySelector('.button__like').addEventListener('click', likeCard);
+    element.querySelector('.elements__image').addEventListener('click', openImage);
+
+    listElements.prepend(element);
 }
 
-//Прикрепляем обработчик при нажатии на кнопки
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
+initialCards.forEach(function(item) {
+    addCard(item.name, item.link);
+});
 
-//Прикрепляем обработчик к форме
-formElement.addEventListener('submit', formSubmitHandler);
 
-//likeButton.addEventListener('click', function (evt) {
-//   evt.target.classList.toggle('elements__button_active');
-//});
+function addFormSubmitHandler (evt) {
+    evt.preventDefault();  
+    let titleInput = document.querySelector('.popup__item_input-title');
+    let linkInput = document.querySelector('.popup__item_input-link');
+    const name = titleInput.value;
+    const link = linkInput.value;
+    titleInput = '';
+    linkInput = '';
+
+    addCard(name, link);
+    togglePopup(addPopup);
+}
+
+formAdd.addEventListener('submit', addFormSubmitHandler);
+
+
+function deleteCard(event) {
+    const element = event.target.closest('.elements__item');
+    element.remove();
+}
+
+
+function likeCard(event) {
+    event.target.classList.toggle('button__like_active');
+}
+
+let imageItem = document.querySelector('.popup__view');
+let imageTitle = document.querySelector('.popup__title_image');
+
+
+//Закрываем попап просмотра фотографии
+let imagePopup = document.querySelector('.popup__image');
+let closeImageView = document.querySelector('.popup__close_image');
+closeImageView.addEventListener('click', function () {
+    togglePopup(imagePopup);
+});
+
+
+function openImage(event) {
+    const element = event.target.closest('.elements__item');
+    imageItem.src = element.querySelector('.elements__image').src;
+    imageTitle.textContent = element.querySelector('.elements__title').textContent;
+    togglePopup(imagePopup);
+}
